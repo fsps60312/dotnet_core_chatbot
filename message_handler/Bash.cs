def536ee3edb59499fdc5c42c0d8e0adeffdb058
fsps60312 <fsps60312@yahoo.com.tw>
2020-06-12 04:37:01 +0800

    improve security

--- message_handler/Bash.cs
+++ message_handler/Bash.cs
@@ -7,44 +7,50 @@ namespace message_handler
 {
     class Bash:DialogNode
     {
+        public static string RunCommand(string command)
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/ssh",
+                    Arguments = "restricted@localhost -p 60313",
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            process.StandardInput.WriteLine(command);
+            process.StandardInput.Close();
+            DateTime start_time = DateTime.Now;
+            while (true)
+            {
+                System.Threading.Thread.Sleep(100);
+                if ((DateTime.Now - start_time).TotalSeconds > 3)
+                {
+                    //SendMsg($"{process.Id} {process.SessionId}");
+                    process.Kill(true);
+                    return "超過3秒囉，卡";
+                }
+                if (process.HasExited)
+                {
+                    string res = process.StandardOutput.ReadToEnd() + process.StandardError.ReadToEnd();
+                    return string.IsNullOrWhiteSpace(res) ? "Oops, no output." : res;
+                }
+            }
+        }
         public override void Run()
         {
             if (sender_msg.ToLower().StartsWith("bash"))
             {
-                var process = new Process()
-                {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "/bin/su",
-                        Arguments = "- test",
-                        RedirectStandardInput = true,
-                        RedirectStandardOutput = true,
-                        RedirectStandardError = true,
-                        UseShellExecute = false,
-                        CreateNoWindow = true,
-                    }
-                };
-                process.Start();
-                process.StandardInput.WriteLine(sender_msg.Substring(4));
-                process.StandardInput.Close();
-                DateTime start_time = DateTime.Now;
-                while (true)
-                {
-                    System.Threading.Thread.Sleep(100);
-                    if ((DateTime.Now - start_time).TotalSeconds > 3)
-                    {
-                        //SendMsg($"{process.Id} {process.SessionId}");
-                        process.Kill(true);
-                        SendMsg("超過3秒囉，卡");
-                        EndDialog(Program.NextDialog);
-                    }
-                    if (process.HasExited)
-                    {
-                        string res = process.StandardOutput.ReadToEnd() + process.StandardError.ReadToEnd();
-                        SendMsg(string.IsNullOrWhiteSpace(res) ? "Oops, no output." : res);
-                        EndDialog(Program.NextDialog);
-                    }
-                }
+                string res = RunCommand(sender_msg.Substring(4).Trim());
+                SendMsg(res);
+                res = RunCommand("killall -9 -u restricted");
+                //Console.WriteLine(res);
+                EndDialog(Program.NextDialog);
             }
         }
     }
